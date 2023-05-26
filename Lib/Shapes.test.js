e4c36444ef8e387078e85829aa6c2afc52c8250f
ibrahim <ibrahimmuhieddine02@gmail.com>
2023-05-25 19:54:50 -0700

    test cases for circle

--- Lib/Shapes.test.js
+++ Lib/Shapes.test.js
@@ -9,3 +9,13 @@ describe('Triangle', () => {
       expect(triangle.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
     });
   });
+
+  describe('Circle', () => {
+    it('should render a circle SVG string with the specified color', () => {
+      const circle = new Circle();
+      circle.setColor('red'); 
+  
+      
+      expect(circle.render()).toEqual('<circle cx="150" cy="100" r="80" fill="red" />');
+    });
+  });
